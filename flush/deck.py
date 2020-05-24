49af4b784f2a4f25ae0861347c81b7963c904d9f
Debesh-XPS <debesh.mandal@hotmail.com>
2020-05-24 01:04:45 +0100

    decks seem to work

--- flush/deck.py
+++ flush/deck.py
@@ -2,23 +2,88 @@ import numpy as np
 import pandas as pd
 from typing import List
 
+class Registry():
+    def __init__(self):
+        self._values = ['A', 'K', 'Q', 'J'] + [str(10-i) for i in range(9)]
+        self._suits = ['hearts', 'spades', 'clubs', 'diamonds']
+        self._hierarchy = dict(zip(self._values, reversed(range(13))))
+
+    @property
+    def values(self):
+        return self._values
+
+    @values.getter
+    def values(self):
+        return self._values
+    
+    @property
+    def suits(self):
+        return self._suits
+    
+    @suits.getter
+    def suits(self):
+        return self._suits
+
+    @property
+    def hierarchy(self):
+        return self._hierarchy
+
+    @hierarchy.getter
+    def hierarchy(self):
+        return self._hierarchy
+
 class Card():
-    def __init__(self, value, suit):
+    def __init__(self, value, suit=None):
+        assert value in Registry().values
+        if suit != None:
+            assert suit in Registry().suits
         self.value = value
         self.suit = suit
+    
+    @property
+    def hierarchy(self):
+        return Registry().hierarchy[self.value]
 
-class CardContainer:
-    def __init__(self, cards : List[Card]):
-        self._cards = cards
-        self._cards_df = pd.DataFrame(cards, columns = ['value', 'suit'])
+    def __ge__(self, other):
+        return self.hierarchy >= other.hierarchy 
+
+    def __ne__(self, other):
+        return self.hierarchy != other.hierarchy
+
+    def __lt__(self, other):
+        return self.hierarchy < other.hierarchy
+
+    def __gt__(self, other):
+        return self.hierarchy > other.hierarchy
+
+    def __le__(self, other):
+        return self.hierarchy <= other.hierarchy
+
+    def __eq__(self, other):
+        return self.hierarchy == other.hierarchy
+
+    def __repr__(self):
+        _suit = '_' if self.suit == None else self.suit
+        return f"{self.value}{_suit}"
+
+class CardContainer(list):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.table = self.dataframe(self)
+
+    @staticmethod
+    def dataframe(cards):
+        df = pd.DataFrame(
+            {
+                'value' : [i.value for i in cards],
+                'suit' : [i.suit for i in cards]
+            }
+        )
+        return df
 
     @property
     def N(self):
-        return len(self.cards)
-
-    @property
-    def cards(self):
-        return self._cards
+        return len(self)
 
     @property
     def clubs(self):
@@ -37,23 +102,28 @@ class CardContainer:
         return self.suit('diamonds')
 
     def suit(self, suit):
-        return
+        return self.table['suit'].str.match(suit).sum()
 
-    def number(self, number):
-        return
+    def count(self, value):
+        return self.table['value'].str.match(value).sum()
 
 class Hand(CardContainer):
     def __init__(self):
-        pass
+        CardContainer.__init__(self, [])
 
 class Deck(CardContainer):
-    def __init__(self):
-        self._cards = Deck.generate()
+    def __init__(self, shuffle=False):
+        CardContainer.__init__(self, self.generate())
+        if shuffle:
+            self.shuffle()
 
     @staticmethod
     def generate():
-        cards = []
-        return cards
+        _cards = []
+        for i in Registry().values:
+            for j in Registry().suits:
+                _cards.append(Card(i, j))
+        return _cards
 
     def shuffle(self):
-        self._cards.shuffle
+        self._cards.shuffle()
---
+++ test/test_deck.py
@@ -0,0 +1,53 @@
+import flush.deck as deck
+
+def test_registry():
+    values = deck.Registry().values
+    suits = deck.Registry().suits
+    hierarchy = deck.Registry().hierarchy
+    assert 'hearts' in suits
+    assert 'spades' in suits
+    assert 'clubs' in suits
+    assert 'diamonds' in suits
+    assert hierarchy['A'] > hierarchy['J']
+
+def test_cards():
+    deck.Card('A', 'spades')
+
+    try:
+        to_fail = deck.Card('ka', 'aido')
+    except AssertionError:
+        pass
+    else:
+        raise AssertionError
+
+    assert deck.Card('A') > deck.Card('K')
+    assert deck.Card('A') >= deck.Card('A')
+    assert deck.Card('K') < deck.Card('A')
+    assert deck.Card('A') <= deck.Card('A')
+    assert deck.Card('A') != deck.Card('K')
+    assert deck.Card('A') == deck.Card('A')
+
+    cards = [
+        deck.Card('2'), 
+        deck.Card('J'), 
+        deck.Card('K'),
+        deck.Card('9'),
+        deck.Card('6')
+    ]
+
+    assert sorted(cards)[0].value == '2'
+
+def test_deck():
+    assert deck.Deck().clubs == 13
+    assert deck.Deck().hearts == 13
+    assert deck.Deck().diamonds == 13
+    assert deck.Deck().spades == 13
+    for value in deck.Registry().values:
+        assert deck.Deck().count(value) == 4
+    assert isinstance(deck.Deck(), list)
+    assert deck.Deck()[0] == deck.Card('A', 'hearts')
+
+if __name__ == '__main__':
+    test_registry()
+    test_cards()
+    test_deck()
